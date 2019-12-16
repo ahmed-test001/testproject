5fb67f577792b36905ec79b5238df29e9324c0e0
ahmed <ahmed@192.168.0.27>
2019-12-15 21:15:20 -0500

    First Commit

---
+++ TestNGProject/src/project2/NewTest.java
@@ -0,0 +1,16 @@
+package project2;
+
+import org.testng.annotations.Test;
+
+
+import java.io.File;
+
+
+public class NewTest {
+	@Test
+	public void test() {
+	System.out.println("print");
+
+  }
+  
+}
